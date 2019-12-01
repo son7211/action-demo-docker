732f8f32edb40249ab00e189e5d092302aebb96f
John Son <40287191+son7211@users.noreply.github.com>
2019-12-01 17:27:06 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,3 @@
 #!/bin/sh
 
 set -e
-
-USERNAME=$1
-PASSWORD=$2
-REPOSITORY=$3
-
-
-if [ -z $USERNAME ]; then
-  echo 'Required username parameter'
-  exit 1
-fi
-
-if [ -z $PASSWORD ]; then
-  echo 'Required password parameter'
-  exit 1
-fi
-
-if [ -z $REPOSITORY ]; then
-  echo 'Required repository parameter'
-  exit 1
-fi
-
-
-
-docker build .
-docker login --u "$USERNAME" --password-stdin "$PASSWORD" $REGISTRY
-docker push 
-
-
