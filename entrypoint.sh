5097df9b2105b4b5e926c7c5276c0ca82f90dfb6
John Son <40287191+son7211@users.noreply.github.com>
2019-12-22 20:01:39 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
 
 set -e
+
+
+if [ -z $USERNAME ]; then
+  echo 'Required username parameter'
+  exit 1
+fi
+
+if [ -z $PASSWORD ]; then
+  echo 'Required password parameter'
+  exit 1
+fi
+
+if [ -z $REPOSITORY ]; then
+  echo 'Required repository parameter'
+  exit 1
+fi
+
+if [[ -z $TAG ]]; then
+  echo 'Tag to snapshot'
+  TAG=$(date '+%Y%m%d%H%M%S')
+fi
+
+
+IMAGE=$REPOSITORY:$TAG
+if [ -n "$REGISTRY" ]; then
+  IMAGE=$REGISTRY/$IMAGE
+fi
+
+docker build -t $IMAGE .
+docker login --username "$USERNAME" --password "$PASSWORD" $REGISTRY
+docker push $IMAGE
+
+echo ::set-output name=image::$IMAGE
